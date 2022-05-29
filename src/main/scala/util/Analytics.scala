c479fe9ab1698db57d15d957879ab08a8036486b
Brod8362 <brod8362@gmail.com>
2022-05-29 15:54:31 -0500

    Add help dialog when added to server

--- src/main/scala/util/Analytics.scala
+++ src/main/scala/util/Analytics.scala
@@ -1,14 +1,32 @@
 package pw.byakuren.nolol
 package util
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.guild.{GuildJoinEvent, GuildLeaveEvent}
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+import java.awt.Color
 
 object Analytics extends ListenerAdapter {
 
   override def onGuildJoin(event: GuildJoinEvent): Unit = {
     guildUpdate(joined = true, event.getGuild)
+
+    val channel = event.getGuild.getDefaultChannel
+    val embed = new EmbedBuilder()
+      .setTitle("Thanks for adding Anti-League!")
+      .setThumbnail(event.getJDA.getSelfUser.getEffectiveAvatarUrl)
+      .setDescription("Before you can start using Anti-League, please visit the [setup guide.](https://github.com/Brod8362/anti-league/wiki/Initial-Setup-Guide)")
+      .addField("Support Server", "https://discord.gg/3Scnd3GvCn", true)
+      .addField("Official Invite Link", "[Click here](https://discord.com/api/oauth2/authorize?client_id=967601110326050816&permissions=2052&scope=bot%20applications.commands)", true)
+      .setColor(Color.ORANGE)
+      .build()
+    try {
+      channel.sendMessageEmbeds(embed).queue()
+    } catch {
+      case _: Throwable =>
+    }
   }
 
   override def onGuildLeave(event: GuildLeaveEvent): Unit = {
